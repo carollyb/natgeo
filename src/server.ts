22cdd94b41e46479e43863749b45ab7169988bdd
carollyb <carolly.barbosa@gmail.com>
2022-05-18 10:40:11 -0300

    :tada: creates user API repo

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+const port = process.env.PORT || 3001
+//import routes from "./routes/index"
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+//app.use(routes)
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "API NatGeo - User Database"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+})
