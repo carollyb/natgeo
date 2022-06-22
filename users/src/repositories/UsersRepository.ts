5f126b58e751c83220c12a6949d4ed794c2cbb4c
carollyb <carolly.barbosa@gmail.com>
2022-06-22 16:05:05 -0300

    :recycle: applies SOLID principles: create user

---
+++ users/src/repositories/UsersRepository.ts
@@ -0,0 +1,9 @@
+export interface UsersRepositoryData {
+    full_name: string,
+    username: string,
+    password: string,
+}
+
+export interface UsersRepository {
+    create: (data: UsersRepositoryData) => Promise<void>
+}
